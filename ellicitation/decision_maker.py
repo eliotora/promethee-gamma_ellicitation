05ecbac7cfb0ed0903c7643d78b02003483978e5
eliotora <eliot.cosyn@gmail.com>
2023-11-01 01:36:11 +0100

    Fixed an error

--- ellicitation/decision_maker.py
+++ ellicitation/decision_maker.py
@@ -7,9 +7,8 @@ class DecisionMaker:
         self.indT = random() / 5
         self.incT = max(0.15, self.indT) + random() / 5
         self.prefF = random() * 5
-        self.w = [random() for _ in range(len(A))]
+        self.w = [random() for _ in range(len(pf))]
         self.w = [i / sum(self.w) for i in self.w]
-
         self.pgInstance = PrometheeGammaInstance(A, self.w, pf, self.indT, self.incT, self.prefF)
         self.pgInstance.compute_preferences()
         self.relations = self.pgInstance.pref
--- ellicitation/elicitationProcedure.py
+++ ellicitation/elicitationProcedure.py
@@ -4,6 +4,7 @@ from promethee_gamma import PrometheeGammaInstance, I, J, P, nP
 from random import random, randint
 import preference_functions as pf
 
+
 def determine_next_query(A, w):
     i = randint(0, len(A))
     j = randint(0, len(A))
@@ -11,25 +12,33 @@ def determine_next_query(A, w):
         j = randint(0, len(A))
     return i, j
 
+
+def next_approx(w, indT, incT, Pf, asked_pref, i, j):
+    return w, indT, incT, Pf
+
+
 def elicitationProcedure():
     # dataset = "data/HDI20_Classic"
     # dataset = "data/SHA_TOP15"
     dataset = "data/HDI20_1and3_quartiles"
     A, w, pref_fct_desc = load_dataset(dataset)
     pref_fct = [pf.make_pref_fct(c["type"], c["ceils"]) for c in pref_fct_desc]
-
+    asked_pref = [[0 for _ in range(len(A))] for _ in range(len(A))]
     dm = DecisionMaker(A=A, pf=pref_fct)
     Pf = random() * 5
     indT = random() / 5
-    incT =  max(0.15, indT) + random() / 5
-    w_approx = [random() for _ in range(len(A))]
+    incT = max(0.15, indT) + random() / 5
+    w_approx = [random() for _ in range(len(pref_fct))]
     w_approx = [i / sum(w_approx) for i in w_approx]
 
     pgammainst = PrometheeGammaInstance(A, w_approx, pref_fct, indT, incT, Pf)
     pgammainst.compute_preferences()
 
     i, j = determine_next_query(A, w)
+    asked_pref[i][j] = dm.query(i, j)
+    print(asked_pref[i][j])
+    w, indT, incT, Pf = next_approx(w, indT, incT, Pf, asked_pref, i, j)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     elicitationProcedure()
